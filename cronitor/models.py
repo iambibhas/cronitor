d592e46e13c73a79e53f79b017532329560ba128
Bibhas <iambibhas@gmail.com>
2016-04-24 04:58:06 +0530

    showing only logs from today on project page

--- cronitor/models.py
+++ cronitor/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.utils import timezone
+import datetime
 
 
 class Project(models.Model):
@@ -11,12 +12,20 @@ class Project(models.Model):
         return self.name
 
 
+class LogManager(models.Manager):
+    def from_today(self):
+        queryset = super(LogManager, self).get_queryset()
+        return queryset.filter(created_at__gte=datetime.date.today())
+
+
 class Log(models.Model):
     project = models.ForeignKey('Project')
     created_at = models.DateTimeField(default=timezone.now)
     ip_address = models.GenericIPAddressField(default='127.0.0.1')
     is_up = models.BooleanField(default=True)
     message = models.TextField(blank=True, null=True)
+
+    objects = LogManager()
 
     def __str__(self):
         return '{} at {} from {}'.format(self.project, self.created_at, self.ip_address)
